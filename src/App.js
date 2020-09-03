7dccdfd4d50a31357c14a5c452ee5325742a3b53
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-03 16:28:06 +1000

    almost finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import Video from './components/Video';
+import axios from './axios';
+
 function App() {
-  return <div className='App'></div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await axios.get('v2/posts');
+      setVideos(response.data);
+      return response;
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className='app'>
+      <div className='app__videos'>
+        {videos.map((video) => (
+          <Video
+            key={video._id}
+            channel={video.channel}
+            description={video.description}
+            song={video.song}
+            shares={video.shares}
+            likes={video.likes}
+            messages={video.messages}
+            url={video.url}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/VideoSidebar.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './VideoSidebar.css';
+import { Favorite, FavoriteBorder, Message, Share } from '@material-ui/icons';
+
+const VideoSidebar = ({ likes, shares, messages }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => setLiked(!liked);
+
+  return (
+    <div className='videoSidebar'>
+      <div className='videoSidebar__button'>
+        {liked ? (
+          <Favorite fontSize='large' onClick={handleLike} />
+        ) : (
+          <FavoriteBorder fontSize='large' onClick={handleLike} />
+        )}
+        <p>{liked ? likes + 1 : likes}</p>
+      </div>
+
+      <div className='videoSidebar__button'>
+        <Message fontSize='large' />
+        <p>{messages}</p>
+      </div>
+
+      <div className='videoSidebar__button'>
+        <Share fontSize='large' />
+        <p>{shares}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoSidebar;
